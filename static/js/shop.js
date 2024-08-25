d66721bff4acf82b46632adf76a3626287aa0081
SergioPinto <38921333+SergioPinto2501@users.noreply.github.com>
2024-08-25 17:27:35 +0200

    Start Shop

---
+++ static/js/shop.js
@@ -0,0 +1,17 @@
+document.getElementById('buy_software').onclick = showShop;
+document.querySelector('.shop-controls .close').onclick = hideShop;
+document.querySelector('.shop-controls .minimize').onclick = hideShop;
+
+function showShop() {
+        div = document.getElementById('shop');
+        div.style.display = 'block';
+}
+function hideShop() {
+        div = document.getElementById('shop');
+        div.style.display = 'none';
+
+}
+function showShop() {
+        div = document.getElementById('shop');
+        div.style.display = 'block';
+}
